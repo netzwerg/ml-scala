9056195e9385a8e348d225791328d46b156b74cf
Rahel Lüthy <rahel.luethy@gmail.com>
2012-11-29 12:19:16 +0100

    Adds stemming; Printing normalized scores

--- src/main/scala/scala/Classify.scala
+++ src/main/scala/scala/Classify.scala
@@ -4,6 +4,7 @@ import io.Source
 import breeze.classify.NaiveBayes
 import breeze.linalg.Counter
 import breeze.data.Example
+import math._
 
 /**
  * Classifies NY Times articles previously fetched via [[scala.NYTimesPull]]. Uses naive
@@ -24,25 +25,36 @@ object Classify extends App {
   // classifying arbitrary sports example
   val sportsTestData = Counter.count("philadelphia", "christmas").mapValues(_.toDouble)
   val classifiedSports = classifier.classify(sportsTestData)
+  println(normalizeScores(classifier.scores(sportsTestData)))
   assert(classifiedSports == Sports)
 
   // classifying arbitrary arts example
   val artsTestData = Counter.count("melodrama", "society").mapValues(_.toDouble)
   val classifiedArts = classifier.classify(artsTestData)
+  println(normalizeScores(classifier.scores(artsTestData)))
   assert(classifiedArts == Arts)
 
   def readExamples(labels: Seq[String]) = {
 
     val langData = breeze.text.LanguagePack.English
     val tokenizer = langData.simpleTokenizer
+    val stemmer = langData.stemmer.getOrElse(identity[String]_)
 
     for {
       label <- labels
       tokens = tokenizer(Source.fromFile(label).mkString).map(_.toLowerCase)
+      tokenStems = tokens.map(stemmer)
       counter = Counter.count[String](tokens).mapValues(_.toDouble)
     } yield {
       Example(label, counter)
     }
+  }
+
+  def normalizeScores(scores: Counter[_,Double]) = {
+    val nonLogScores = scores.mapValues(-exp(_))
+    val scoreSum = nonLogScores.sum
+    val normalizedScores = nonLogScores.mapValues(_ / scoreSum)
+    normalizedScores
   }
 
 }

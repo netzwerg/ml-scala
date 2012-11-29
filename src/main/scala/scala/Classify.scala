c526af39e13f2d469565fcdd00703367ffb28fe1
Rahel Lüthy <rahel.luethy@gmail.com>
2012-11-29 18:07:43 +0100

    Improved test data and output (including normalized probabilities)

--- src/main/scala/scala/Classify.scala
+++ src/main/scala/scala/Classify.scala
@@ -14,6 +14,8 @@ import math._
  */
 object Classify extends App {
 
+  val PrettyPrint = "\n%s \nClassification probability for '%s'\n%s"
+
   // label constants (also corresponding to file names)
   val Arts = "arts"
   val Sports = "sports"
@@ -22,35 +24,39 @@ object Classify extends App {
   val trainingData = readExamples(List(Arts, Sports))
   val classifier = new NaiveBayes.Trainer().train(trainingData)
 
-  // classifying arbitrary sports example
-  val sportsTestData = Counter.count("philadelphia", "christmas").mapValues(_.toDouble)
-  val classifiedSports = classifier.classify(sportsTestData)
-  println(normalizeScores(classifier.scores(sportsTestData)))
-  assert(classifiedSports == Sports)
+  // classifying arbitrary CNN sports example
+  val SportsSentence = "Though Gang Green's fans may have reached a breaking point, Bart Scott isn't willing to let them say whatever they want."
+  val sportsTestData = createCounter(SportsSentence)
+  assert(classifier.classify(sportsTestData) == Sports)
+  val classifiedSportsProb = normalizeScores(classifier.scores(sportsTestData)).get(Sports).get
+  println(PrettyPrint.format(Sports.toUpperCase, SportsSentence, classifiedSportsProb))
 
-  // classifying arbitrary arts example
-  val artsTestData = Counter.count("melodrama", "society").mapValues(_.toDouble)
-  val classifiedArts = classifier.classify(artsTestData)
-  println(normalizeScores(classifier.scores(artsTestData)))
-  assert(classifiedArts == Arts)
+  // classifying arbitrary Guardian arts example
+  val ArtsSentence = "Monet painted the olive trees and described the gardens at Villa Mariani in Bordighera, Italy, as 'pure magic.'"
+  val artsTestData = createCounter(ArtsSentence)
+  assert(classifier.classify(artsTestData) == Arts)
+  val classifiedArtsProb = normalizeScores(classifier.scores(artsTestData)).get(Arts).get
+  println(PrettyPrint.format(Arts.toUpperCase, ArtsSentence, classifiedArtsProb))
 
   def readExamples(labels: Seq[String]) = {
-
-    val langData = breeze.text.LanguagePack.English
-    val tokenizer = langData.simpleTokenizer
-    val stemmer = langData.stemmer.getOrElse(identity[String]_)
-
     for {
       label <- labels
-      tokens = tokenizer(Source.fromFile(label).mkString).map(_.toLowerCase)
-      tokenStems = tokens.map(stemmer)
-      counter = Counter.count[String](tokens).mapValues(_.toDouble)
+      counter = createCounter(Source.fromFile(label).mkString)
     } yield {
       Example(label, counter)
     }
   }
 
-  def normalizeScores(scores: Counter[_,Double]) = {
+  def createCounter(s: String) = {
+    val langData = breeze.text.LanguagePack.English
+    val tokenizer = langData.simpleTokenizer
+    val stemmer = langData.stemmer.getOrElse(identity[String] _)
+    val tokens = tokenizer(s).map(_.toLowerCase)
+    val tokenStems = tokens.map(stemmer)
+    Counter.count[String](tokenStems).mapValues(_.toDouble)
+  }
+
+  def normalizeScores(scores: Counter[String, Double]) = {
     val nonLogScores = scores.mapValues(-exp(_))
     val scoreSum = nonLogScores.sum
     val normalizedScores = nonLogScores.mapValues(_ / scoreSum)
